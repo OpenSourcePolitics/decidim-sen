a6108d57e77599f6596c33bef0f3db021ee908e1
armandfardeau <fardeauarmand@gmail.com>
2022-12-19 12:17:53 +0100

    Rename queue

--- app/jobs/check_anonymizable_initiatives_job.rb
+++ app/jobs/check_anonymizable_initiatives_job.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class CheckAnonymizableInitiativesJob < ApplicationJob
+  queue_as :scheduled
   unique :while_executing, on_conflict: :log
-  queue_as :anonymize_initiatives
 
   def perform
     Decidim::CheckAnonymizableInitiativesService.run
